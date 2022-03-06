6c2f631a1c7287cbfef6c721c00aaaca08578d6c
Julien Grob <Grob.julien1@gmail.com>
2022-03-06 17:53:58 +0100

    Test "hello world"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         QuadraticEquation equ = new QuadraticEquation();
         equ.calculSolutions();
+        System.out.println("Hello wold");
     }
 }
